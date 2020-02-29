de392ef0137c8690996cba00365afc6d9a2aa9c4
mmorito <morito@mnes.org>
2020-03-01 01:14:16 +0900

    add: '@nuxt/typescript-build'

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   /*
    ** Nuxt.js dev-modules
    */
-  buildModules: ["@nuxtjs/vuetify"],
+  buildModules: ["@nuxt/typescript-build", "@nuxtjs/vuetify"],
   /*
    ** Nuxt.js modules
    */
